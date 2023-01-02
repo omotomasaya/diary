334312fd28398e46d863326939efffc180e37fa7
omotomasaya <omotomasayaarai@gmail.com>
2023-01-02 22:14:45 +0900

    first commit

---
+++ config/unicorn.rb
@@ -0,0 +1,15 @@
+$worker  = 2
+$timeout = 30
+$app_dir = "/myproject"
+$listen  = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
+$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+$stdout_log = File.expand_path 'log/unicorn.stdout.log', $app_dir
+$stderr_log = File.expand_path 'log/unicorn.stderr.log', $app_dir
+
+worker_processes $worker
+working_directory $app_dir
+stdout_path $stdout_log
+stderr_path $stderr_log
+timeout $timeout
+listen $listen
+pid $pid
