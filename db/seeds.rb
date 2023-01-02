5db2a0c680ea1466d283adb58adbf5a5cbd66fb1
omotomasaya <omotomasayaarai@gmail.com>
2023-01-03 02:26:43 +0900

    seed 修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-AdminUser.find_or_create_by(
+AdminUser.find_or_create_by!(
   email: "admin@example.com",
-  encrypted_password: "password1",
-  reset_password_token: "password1",
   last_name: "山田",
   first_name: "太郎",
   last_kana_name: "ヤマダ",
   first_kana_name: "タロウ"
-)
+) do |user|
+  user.password = "password1"
+  user.password_confirmation = "password1"
+end
