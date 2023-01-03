c638320764b3dd30a1b7c5d3cab4dc7b447e2287
omotomasaya <omotomasayaarai@gmail.com>
2023-01-03 09:05:10 +0900

    application から sessions にメソッド移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
-    admin_dashboards_path
-  end
-
-  def after_sign_out_path_for(resource)
-    new_admin_user_session_path
-  end
 end
