929192b1e89324a1c6e08dbefe47410fed2a9b79
omotomasaya <omotomasayaarai@gmail.com>
2023-01-04 23:00:55 +0900

    dbにnotificationsを作成

---
+++ db/migrate/20230104120323_create_notifications.rb
@@ -0,0 +1,12 @@
+class CreateNotifications < ActiveRecord::Migration[7.0]
+  def change
+    create_table :notifications do |t|
+      t.string :title, null: false
+      t.text :content, null: false
+      t.datetime :start_datetime, null: false
+      t.datetime :end_datetime
+      t.boolean :status, null: false, default: false
+      t.timestamps
+    end
+  end
+end
